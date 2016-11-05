b21acc7123fefb4a5edc81b59b689270a3832d36
Nick Oki <nick.oki@live.com>
2016-11-05 17:45:53 -0400

    Add links and images to index route

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,8 +6,8 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
     respond_to do |format|
-      format.html { }
-      format.json { render(json: @projects ) }
+      format.html
+      format.json { render( json: @projects, :include => [:links, :images]  ) }
     end
   end
 
